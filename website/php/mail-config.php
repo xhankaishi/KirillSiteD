721e582c64b84b1e14241c23120efe98e3857342
xhankaishi <159677636+xhankaishi@users.noreply.github.com>
2024-06-06 00:44:06 +0600

    Запус
    Запуск

---
+++ website/php/mail-config.php
@@ -0,0 +1,11 @@
+<?php 
+
+// put your email address here
+define('YOUR_EMAIL_ADDRESS', 'mail@themeinjection.com'); 
+define('YOUR_COMPANY_NAME', 'Car|Rental Landing Page'); 
+
+$mailchimpSupport 	= true; // set 'true' to activate mailchimp support
+
+define('MAILCHIMP_API_KEY', 'b714582d55c439ece1f49cb8d167c4b9-us3');
+define('MAILCHIMP_LIST_ID', 'f84fe4d84c');
+
